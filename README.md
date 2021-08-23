f00bb37e12422f8c004ea72fc811850d2f360ea3
Sam <sathiyamurthi239@gmail.com>
2021-08-23 11:55:02 +0530

    updated

---
+++ README.md
@@ -0,0 +1,9 @@
+Instructions to run the program:
+
+-> Store front and side images in the 'in' folder in the repo directory.
+-> cd into hmr2/src
+-> Run download_model.py if running for the first time.
+-> Run final.py and enter height. Enter height as 0 to stop the program.
+-> Output is stored in the 'out' folder in the repo directory.
+-> accuracy.py in the repo directory can be used to check accuracy of the calculation if actual measurements 
+    are stored in the actual_measure.json file in the 'in' folder.
--- accuracy.py
+++ accuracy.py
@@ -10,14 +10,14 @@ with open('in\\actual_measure.json','r') as out:
 del y_pred['Height']
 del y_pred['feedback']
 # y_true =  {'Waist': 95, 'Neck': 38, 'Cuff': 18, 'Shoulder': 50, 'Arm': 60}
-
+name = input("Name of sample: ")
 
 mse = mean_squared_error(list(y_true.values()), list(y_pred.values()), sample_weight=None, multioutput='uniform_average', squared=True)
 mape = mean_absolute_percentage_error(list(y_true.values()), list(y_pred.values()), sample_weight=None, multioutput='uniform_average')
 print("MSE: " + str(mse))
 print("ACC (using MAPE): " + str((1 - mape)*100) + " %")
 
-acc = [mse, (1 - mape)*100]
+acc = [name, mse, (1 - mape)*100]
 from csv import writer
 def append_list_as_row(file_name, list_of_elem):
     # Open file in append mode
